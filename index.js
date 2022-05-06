9704bace3df52091b2dc5934d462a0023bd2266c
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-07 01:45:25 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fjfcd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('mongodb connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send('running perfume server');
