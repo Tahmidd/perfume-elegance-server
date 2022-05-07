7ccce7b15a5e10145d29777036a8ab083e5b115b
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-08 01:22:41 +0600

    single inventory show added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 //middleware
@@ -19,12 +19,19 @@ async function run() {
         await client.connect();
         const productCollection = client.db('perfumeElegance').collection('product');
 
-        app.get('/product', async (req, res) => {
+        app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
 
     }
     finally {
